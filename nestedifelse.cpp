f8148e8064b3b856d79ad92f798570c39f7d08b4
markgicharu <markgicharu@hotmail.com>
2016-02-02 20:01:54 +0300

    Update nestedifelse.cpp

--- nestedifelse.cpp
+++ nestedifelse.cpp
@@ -13,28 +13,28 @@ int main()
   cout<< "======Please enter your grades below====="<<endl;
   cin>> mark;
   //conditional statement
-  if(mark >= 0 && score<= 40)
+  if(mark >= 0 && mark<= 40)
   {
     cout<<"F"<<endl;
   }
   else
-  if(mark >= 41 && score<= 60)
+  if(mark >= 41 && mark<= 60)
   {
     cout<<"C"<<endl;
   }
   else
-  if(mark >= 61 && score<= 80)
+  if(mark >= 61 && mark<= 80)
   {
     cout<<"B"<<endl;
   }
   else
-  if(mark >= 81 && score<= 100)
+  if(mark >= 81 && mark<= 100)
   {
     cout<<"A"<<endl;
   }
   else
   {
-  cout<<"Enter a grade between 0 and 100"<<end1;
+  cout<<"Enter a grade between 0 and 100"<<endl;
   }
   return 0;
 }
