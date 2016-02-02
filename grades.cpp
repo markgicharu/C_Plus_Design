a07a9e3be155f61b1605b3319769395789918dc0
markgicharu <markgicharu@hotmail.com>
2016-02-02 18:50:07 +0300

    Update grades.cpp

--- grades.cpp
+++ grades.cpp
@@ -1,1 +1,17 @@
+//preprocessor directive
+# include <iostream>
+using namespace std;
+
+//mainfunction
+
+int main()
+{
+  //declare variables here
+  int mark;
+  
+  //prompt the user to enter the grades
+  cout<< "======Please enter your grades below=====""\;
+  cin>>
+  
+}
 
