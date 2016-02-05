0abb5881f7536e1e64c1ca9c037c922d2146dd69
markgicharu <markgicharu@hotmail.com>
2016-02-05 17:31:50 +0300

    Update age.cpp

--- age.cpp
+++ age.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  //declarevariables
+  int MY_AGE;
+  //initiate process
+  cout<<"Press a character and press Enter:";
+  cin>>MY_AGE;
+  cout<<MY_AGE;
+
+  return 0;
+  //end of process
+
+}
