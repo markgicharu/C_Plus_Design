b8f7de533183696937ac159e1e417943ed673193
markgicharu <markgicharu@hotmail.com>
2016-02-05 17:42:30 +0300

    Update number3.cpp

--- Week1Exercise1/number3.cpp
+++ Week1Exercise1/number3.cpp
@@ -1,1 +1,17 @@
+//preprocessor directive
+#include <iostream>
 
+using namespace std;
+
+//mainfunction
+int main ()
+{
+  //declare variables
+  int t = 10;
+  int f = 15;
+  float w = 12.6;
+  //process starts here
+  cout<< 
+  
+  
+}
